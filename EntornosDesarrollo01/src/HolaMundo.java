c647dd4be9c28eb76dfa23313e25fa6e1b38a0a8
apene <apene@LAPTOP-KL04947A>
2017-01-16 22:33:26 +0100

    Mi Hola Mundo!!
    Desarrollado sobre el bloc de notas y compilado en la consola

---
+++ EntornosDesarrollo01/src/HolaMundo.java
@@ -0,0 +1,11 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// mostramos un mensaje de texto en pantalla
+		
+		System.out.println("Hola a los alumnso de DAM");
+
+	}
+
+}
