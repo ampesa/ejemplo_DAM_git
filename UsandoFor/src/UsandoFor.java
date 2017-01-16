72e10300a77d530893874edfe5b482bf366a163b
apene <apene@LAPTOP-KL04947A>
2017-01-16 15:58:33 +0100

    Ejemplo de uso e for

---
+++ UsandoFor/src/UsandoFor.java
@@ -0,0 +1,18 @@
+
+public class UsandoFor {
+
+	public static void main(String[] args) {
+		// Estructura de control for
+		// inicialización; condicion; paso
+		for (int i=0;i<10;i++){
+			System.out.println(i);
+		}
+		for (int i=10;i>0;i--){
+			int j=3;
+			System.out.println("Variable i:"+i);
+			System.out.println("Variable j:"+j);
+		}
+
+	}
+
+}
